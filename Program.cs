77535aa6ead9ba7af4f6442e1e117f673989bfbd
OlegB <os.babkin@gmail.com>
2022-06-30 21:11:37 +0300

    Добавил блок-схему

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+string[] array =
+{
+    "hello",
+    "2222",
+    "world",
+    "))):)",
+    "DataScience",
+    "St-Petersburg"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+    else
+    {
+        Console.WriteLine("Ничоге не вывидина :(");
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
